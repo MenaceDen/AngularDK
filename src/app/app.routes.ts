e7e9464f15fbcbd1964750c31e573337b7768899
Denis Menace <trideks@sbb.rs>
2024-07-06 00:53:37 +0200

    shop added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ShopComponent } from './shop/shop.component';
+import { FooterComponent } from './footer/footer.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'shop', component: ShopComponent },
+  { path: 'contact', component: FooterComponent },
+  { path: '**', redirectTo: '/not-found' },
+];
