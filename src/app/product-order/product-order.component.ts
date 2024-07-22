9998594cd64dc39bd45129aae3d2d87a4e9d9aee
Denis Menace <trideks@sbb.rs>
2024-07-22 14:00:38 +0200

    reactive form finiched

--- src/app/product-order/product-order.component.ts
+++ src/app/product-order/product-order.component.ts
@@ -1,12 +1,46 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { FormsModule, NgForm } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { WarehouseService } from '../services/warehouse.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-product-order',
   standalone: true,
-  imports: [FormsModule, CommonModule],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './product-order.component.html',
   styleUrl: './product-order.component.css',
 })
-export class ProductOrderComponent {}
+export class ProductOrderComponent implements OnInit {
+  constructor(
+    private warehouseService: WarehouseService,
+    private router: Router
+  ) {}
+  orderForm!: FormGroup;
+  ngOnInit(): void {
+    this.orderForm = new FormGroup({
+      nameField: new FormControl(null, Validators.required),
+      surnameField: new FormControl(null, Validators.required),
+      emailField: new FormControl(null, [
+        Validators.email,
+        Validators.required,
+      ]),
+      phoneField: new FormControl(null, Validators.required),
+      addressField: new FormControl(null, Validators.required),
+      commentField: new FormControl(null),
+      paymentField: new FormControl(null, Validators.required),
+    });
+  }
+  onSubmit() {
+    console.log(this.orderForm);
+    this.warehouseService.cartItems.length = 0;
+    this.warehouseService.quantityToShow = 0;
+    this.warehouseService.showOnTheNav(0);
+    this.router.navigate(['/home']);
+  }
+}
